45d5c4bf4ff2f2f655063ce2cc94f56759ec6bd5
lela0 <lela0@10.69.190.166>
2022-12-02 17:40:15 -0500

    Did User class

--- adProject/src/setup/ClassandQuestion.java
+++ adProject/src/setup/ClassandQuestion.java
@@ -1,24 +1,25 @@
 package setup;
 
+import java.util.ArrayList;
+
 public class ClassandQuestion {
 	
 	public static void main(String[] args) {
 		
 		String[] questions = new String[5];
 		
+		// Question options
 		questions[0] = "In what city were you born?";
 		questions[1] = "What is the name of your favorite pet?";
 		questions[2] = "What is your mother's maiden name?";
 		questions[3] = "What high school did you attend?";
 		questions[4] = "What was the name of your elementary school?";
 		
-		
-
 	}
 
 }
 
-abstract public class User {
+abstract class User {
 	
 	private String firstName;
 	private String lastName;
@@ -32,6 +33,63 @@ abstract public class User {
 	private int securityQuestion;
 	private string answer;
 	
+	ArrayList<Integer> flights = new ArrayList<>();
 	
+	protected User() {
+	}
+	
+	// all setting up variables 
+	public void setFirst(String h) {
+		this.firstName = h;	
+	}
+	
+	public void setLast(String j) {
+		this.lastName = j;
+	}
+	
+	public void setAddress(String k) {
+		this.address = k;
+	}
+	
+	public void setZip(int l) {
+		this.zipCode = l;
+	}
+	
+	public void setState(String a) {
+		this.state = a;
+	}
+	
+	public void setUsername(String s) {
+		this.username = s;
+	}
+	
+	public void setPassword(String d) {
+		this.password = d;
+	}
+	
+	public void setEmail(String f) {
+		this.email = f;
+	}
+	
+	public void setSSN(int g) {
+		this.ssn = g;
+	}
+	
+	public void whichQuestion(int q) {
+		this.securityQuestion = q;
+	}
+	
+	public void setAnswer(String w) {
+		this.answer = w;
+	}
+	
+	// Flights on account
+	public void bookFlight(int e) {
+		flights.add(e);
+	}
+	
+	public void removeFlight(int r) {
+		flights.remove(r);
+	}
 	
 }
