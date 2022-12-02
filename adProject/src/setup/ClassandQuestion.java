337281c9f796baeac8b04de525f2e97006179868
lela0 <lela0@10.69.190.166>
2022-12-02 14:54:41 -0500

    Started

---
+++ adProject/src/setup/ClassandQuestion.java
@@ -0,0 +1,37 @@
+package setup;
+
+public class ClassandQuestion {
+	
+	public static void main(String[] args) {
+		
+		String[] questions = new String[5];
+		
+		questions[0] = "In what city were you born?";
+		questions[1] = "What is the name of your favorite pet?";
+		questions[2] = "What is your mother's maiden name?";
+		questions[3] = "What high school did you attend?";
+		questions[4] = "What was the name of your elementary school?";
+		
+		
+
+	}
+
+}
+
+abstract public class User {
+	
+	private String firstName;
+	private String lastName;
+	private String address;
+	private int zipCode;
+	private String state;
+	private String username;
+	private String password;
+	private String email;
+	private int ssn;
+	private int securityQuestion;
+	private string answer;
+	
+	
+	
+}
